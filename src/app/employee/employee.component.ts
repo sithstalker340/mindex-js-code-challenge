c059d9536d63af30e941ee0c494af3606895ce9b
sithstalker340 <aaudycki@gmail.com>
2018-06-13 14:16:19 -0400

    Added the compensation property of type number to employee.ts. Added a new field to employee.component.ts in order to display the total number of employees that report to an employee, both directly and indirectly.

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,35 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input,OnInit} from '@angular/core';
 
 import {Employee} from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Observable, from } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit{
   @Input() employee: Employee;
-
-  constructor() {
+  compensation: number;
+  totalEmployeeReports: number;
+  
+  constructor(private employeeService: EmployeeService) {
+	  this.totalEmployeeReports = 0;
+  }
+  ngOnInit(){
+	this.setTotalReports(this.employee);
+  }
+  setTotalReports(employee: Employee){
+	if(this.employee && employee.directReports){
+		this.totalEmployeeReports += employee.directReports.length;
+		from(employee.directReports).pipe(
+			flatMap(id => <Observable<Employee>> 
+			this.employeeService.get(id))
+		).subscribe(
+			nextEmployee => this.setTotalReports(nextEmployee)
+		);
+	}
   }
 }
